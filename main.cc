7ccb725b2e375b970a0b915b7a342baa9251c010
omidrazzaghi <omidrazzaghi20000@gmail.com>
2024-05-25 12:41:16 +0330

    [TEST]: test raw socket receiver

--- main.cc
+++ main.cc
@@ -1,7 +1,14 @@
 #include <iostream>
 
+#include <receiver_socket/receiver_socket.hh>
+
 int main(){
 
     std::cout << "In the name of Allah" << std::endl;
+
+    ReceiverSocket recv1 = ReceiverSocket("wlx3460f954dcac");
     
+    recv1.StartReceiving();
+
+    return 0;
 }
---
+++ receiver_socket/src/receiver_socket.cc
@@ -0,0 +1,66 @@
+#include "receiver_socket/receiver_socket.hh"
+
+void ReceiverSocket::StartReceiving()
+{
+
+	/* configure for initilizing module*/
+	isActive = true;
+
+
+	int ret, i;
+	int sockopt;
+	ssize_t numbytes;
+	struct ifreq ifopts; /* set promiscuous mode */
+	struct ifreq if_ip;	 /* get ip addr */
+	struct sockaddr_storage their_addr;
+	uint8_t buf[SIZE_OF_BUFFERS];
+	char ifName[IFNAMSIZ];
+
+	/* Header structures */
+	struct ether_header *eh = (struct ether_header *)buf;
+
+	strcpy(ifName, interface_name);
+	/* Open PF_PACKET socket, listening for EtherType ETHER_TYPE */
+	if ((sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1)
+	{
+		perror("listener: socket");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Set interface to promiscuous mode - do we need to do this every time? */
+	strncpy(ifopts.ifr_name, ifName, IFNAMSIZ - 1);
+	ioctl(sockfd, SIOCGIFFLAGS, &ifopts);
+	ifopts.ifr_flags |= IFF_PROMISC;
+	ioctl(sockfd, SIOCSIFFLAGS, &ifopts);
+	/* Allow the socket to be reused - incase connection is closed prematurely */
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof sockopt) == -1)
+	{
+		perror("setsockopt");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+	/* Bind to device */
+	if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, ifName, IFNAMSIZ - 1) == -1)
+	{
+		perror("SO_BINDTODEVICE");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+	int counter = 0;
+	sockaddr_ll address;
+	socklen_t len = sizeof(address);
+
+	while (isActive)
+	{
+		numbytes = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&address, &len);
+
+		printf("listener: got packet %lu bytes\n", numbytes);
+
+		if (address.sll_pkttype == PACKET_OUTGOING)
+		{
+
+			continue;
+		}
+	}
+
+}
